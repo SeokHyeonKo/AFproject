3066109154e9763e49a4bff2f1c0e49b87d3d73e
ko <go@marusoft.net>
2016-06-20 17:04:37 +0900

    fisrt test

---
+++ app/src/main/java/yesman/af/softwareengineeringdepartment/cbnu/yesmanproject/MainActivity.java
@@ -0,0 +1,14 @@
+package yesman.af.softwareengineeringdepartment.cbnu.yesmanproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
